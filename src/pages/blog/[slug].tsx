76e044d8072a1933c507c4ec1bb4b5013fa04ad0
landtry <landry@boldist.co>
2022-08-27 17:41:01 -0500

    added post component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,60 +1,28 @@
-import groq from "groq";
-import { PortableText } from "@portabletext/react";
-import { urlFor } from "@/utils/sanity";
-import Image from "next/image";
 import { trpc } from "@/utils/trpc";
+import Post from "@/components/Post";
+import { Header } from "@/components/Header";
 
-// const ptComponents = {
-//   types: {
-//     image: ({ value }: any) => {
-//       if (!value?.asset?._ref) {
-//         return null;
-//       }
-//       return (
-//         // eslint-disable-next-line @next/next/no-img-element
-//         <img
-//           alt={value.alt || " "}
-//           loading="lazy"
-//           src={urlFor(value).width(320).height(240).fit("max").auto("format")}
-//         />
-//       );
-//     },
-//   },
-// };
+const PostPage = ({ slug }: any) => {
+  const postTRPC = trpc.useQuery(["post.find-unique", { slug }]);
 
-const Post = ({ post }: any) => {
-  const posts = trpc.useQuery(["post.find-unique"]);
+  if (postTRPC.isLoading) return <div></div>;
 
-  return <p>{posts.data?.title}</p>;
+  return (
+    <>
+      <Header />
+      <Post post={postTRPC.data} />
+    </>
+  );
 };
 
-// const query = groq`*[_type == "post" && slug.current == $slug][0]{
-//   title,
-//   "name": author->name,
-//   "categories": categories[]->title,
-//   "authorImage": author->image,
-//   body
-// }`;
-// export async function getStaticPaths() {
-//   const paths = await client.fetch(
-//     groq`*[_type == "post" && defined(slug.current)][].slug.current`
-//   );
+export async function getServerSideProps(context: any) {
+  // It's important to default the slug so that it doesn't return "undefined"
+  const { slug } = context.params;
+  return {
+    props: {
+      slug,
+    },
+  };
+}
 
-//   return {
-//     paths: paths.map((slug) => ({ params: { slug } })),
-//     fallback: true,
-//   };
-// }
-
-// export async function getStaticProps(context) {
-//   // It's important to default the slug so that it doesn't return "undefined"
-//   const { slug = "" } = context.params;
-//   const post = await client.fetch(query, { slug });
-//   return {
-//     props: {
-//       post,
-//     },
-//   };
-// }
-
-export default Post;
+export default PostPage;
