4bddf43b54587790f56ba730e486035ca49969fe
landtry <landry@boldist.co>
2022-08-27 15:33:20 -0500

    added Sanity Image Loader for thumbail images

--- src/components/FeaturedPostCard.tsx
+++ src/components/FeaturedPostCard.tsx
@@ -1,27 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import SanityImage from "./SanityImageLoader";
 
-function FeaturedPostCard({
-  title,
-  slug,
-  author,
-  imageUrl,
-  description,
-  publishedAt,
-}: any) {
+function FeaturedPostCard({ post }: any) {
   return (
     <div
-      key={title}
+      key={post.title}
       className="flex flex-col rounded-lg shadow-lg overflow-hidden">
       <div className="flex-shrink-0">
-        {/* <Image
-          className="h-48 w-full object-cover"
-          src={imageUrl}
-          width="600"
-          height="300"
-          alt=""
-        /> */}
+        <SanityImage src={post.image} />
       </div>
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
@@ -30,17 +18,19 @@ function FeaturedPostCard({
               {category.name}
             </a> */}
           </p>
-          <Link href={`/blog/${slug.current}`}>
+          <Link href={`/blog/${post.slug.current}`}>
             <a className="block mt-2">
-              <p className="text-xl font-semibold text-gray-900">{title}</p>
-              <p className="mt-3 text-base text-gray-500">{description}</p>
+              <p className="text-xl font-semibold text-gray-900">
+                {post.title}
+              </p>
+              <p className="mt-3 text-base text-gray-500">{post.description}</p>
             </a>
           </Link>
         </div>
         <div className="mt-6 flex items-center">
           <div className="flex-shrink-0">
-            <a href={author.href}>
-              <span className="sr-only">{author.name}</span>
+            <a href={post.author.href}>
+              <span className="sr-only">{post.author.name}</span>
               {/* <Image
                 className="h-10 w-10 rounded-full"
                 src={author.imageUrl}
@@ -52,16 +42,16 @@ function FeaturedPostCard({
           </div>
           <div className="ml-3">
             <p className="text-sm font-medium text-gray-900">
-              <a href={author.href} className="hover:underline">
-                {author.name}
+              <a href={post.author.href} className="hover:underline">
+                {post.author.name}
               </a>
             </p>
             <div className="flex space-x-1 text-sm text-gray-500">
               <time dateTime={"datetime"}>
-                {new Date(publishedAt).toDateString()}
+                {new Date(post.publishedAt).toDateString()}
               </time>
-              <span aria-hidden="true">&middot;</span>
-              <span>{"readingTime"} read</span>
+              {/* <span aria-hidden="true">&middot;</span>
+              <span>{"readingTime"} read</span> */}
             </div>
           </div>
         </div>
