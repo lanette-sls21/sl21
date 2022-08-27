c5fa84eeffc53e4069e78e730d7386a35e7fb1ef
landtry <landry@boldist.co>
2022-08-27 17:39:04 -0500

    updated sanity import to default import

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,4 +1,4 @@
-import { sanity } from "@/server/sanity/client";
+import sanity from "@/server/sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export function urlFor(source: any) {
