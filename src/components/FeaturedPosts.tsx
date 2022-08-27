9980cacfd4fd7c6bea8539d41b007ecd02f6076a
landtry <landry@boldist.co>
2022-08-27 18:10:33 -0500

    removed console.log()

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -4,8 +4,6 @@ import FeaturedPostCard from "./FeaturedPostCard";
 export default function FeaturedPosts() {
   const posts = trpc.useQuery(["post.find-many"]);
 
-  console.log(posts.data);
-
   return (
     <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
       <div className="absolute inset-0">
