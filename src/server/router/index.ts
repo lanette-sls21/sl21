4a7225559c803080f7ff05353fe75de5d0651df4
landtry <landry@boldist.co>
2022-08-27 20:24:50 -0500

    imported primary features route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,11 +3,13 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 import { authRouter } from "./auth";
 import { postRouter } from "./post";
+import { primaryFeatureRouter } from "./primaryFeature";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("auth.", authRouter)
-  .merge("post.", postRouter);
+  .merge("post.", postRouter)
+  .merge("primaryFeature.", primaryFeatureRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
