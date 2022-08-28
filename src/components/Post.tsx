aa0b597a6e1c6d23ba1ee1b60dbb6549e2228ca8
landtry <landry@boldist.co>
2022-08-28 07:11:29 -0500

    added postloader component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,7 @@
-import { urlFor } from "@/utils/sanity";
 import { PortableText } from "@portabletext/react";
-import Image from "next/image";
+import PostLoader from "./PostLoader";
 import SanityImage from "./SanityImageLoader";
-import SvgDots from "./svgDots";
+import SvgDots from "./SvgDots";
 
 const ptComponents = {
   types: {
@@ -23,21 +22,25 @@ export default function Post({ post }: any) {
   return (
     <div className="relative py-16 bg-white overflow-hidden">
       <SvgDots />
-      <div className="relative px-4 sm:px-6 lg:px-8">
-        <div className="text-lg max-w-prose mx-auto">
-          <span className="block text-lg text-center text-blue-600 font-semibold">
-            Article
-          </span>
-          <h1>
-            <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl capitalize">
-              {post.title}
+      {!post ? (
+        <PostLoader />
+      ) : (
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto">
+            <span className="block text-lg text-center text-blue-600 font-semibold">
+              Article
             </span>
-          </h1>
-          <article className="mt-6 prose prose-blue  prose-a:text-blue-600 prose-strong:text-blue-600 prose-lg text-gray-500 mx-auto prose-img:rounded-xl">
-            <PortableText value={post.body} components={ptComponents} />
-          </article>
+            <h1>
+              <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl capitalize">
+                {post.title}
+              </span>
+            </h1>
+            <article className="mt-6 prose prose-blue  prose-a:text-blue-600 prose-strong:text-blue-600 prose-lg text-gray-500 mx-auto prose-img:rounded-xl">
+              <PortableText value={post.body} components={ptComponents} />
+            </article>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
