1d417ef08b53007c798c4892f4e25d038535264d
landtry <landry@boldist.co>
2022-08-28 04:36:03 -0500

    imported testimonial router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,12 +4,14 @@ import superjson from "superjson";
 import { authRouter } from "./auth";
 import { postRouter } from "./post";
 import { primaryFeatureRouter } from "./primaryFeature";
+import { testimonialRouter } from "./testimonial";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("auth.", authRouter)
   .merge("post.", postRouter)
-  .merge("primaryFeature.", primaryFeatureRouter);
+  .merge("primaryFeature.", primaryFeatureRouter)
+  .merge("testimonail.", testimonialRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
